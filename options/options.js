af6bf7565a4e735ecc7c3b5a045e8383673588a2
Crowdbotics <team@crowdbotics.com>
2021-08-15 18:21:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "chnge_29829Identifier",
+  url: "https://chnge_29829Identifier.botics.co",
+  api: "https://chnge_29829Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
